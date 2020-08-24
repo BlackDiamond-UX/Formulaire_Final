10df3fe6552708cb5f137b2f2b4c0f0199270485
BlackDIamond-UX <rouiha594@gmail.com>
2020-08-24 11:02:41 +0100

    update

--- js/script.js
+++ js/script.js
@@ -1,31 +1,50 @@
-const consulte_1 = document.getElementById("consulte-A");
-const consulte_2 = document.getElementById("consulte-B");
-const consulte_3 = document.getElementById("consulte-C");
-const constu1 = document.getElementById("activities1")
-const constu2 = document.getElementById("activities2")
-const constu3 = document.getElementById("activities3")
+const activitie = document.getElementById('activities');
 
 function Evnt(X) {
+    if (X == 'Travaux') {
 
-    if (X == 'consulte-A') {
+        document.getElementById("Travaux-dentaire").style.display = 'block';
+        document.getElementById("Consultation-normale").style.display = 'none';
+        document.getElementById("soin-dentaire").style.display = 'none';
+        document.getElementById("Garantie").style.display = 'block';
 
-        constu1.style.display = 'none';
-        constu2.style.display = 'block';
-        constu3.style.display = 'none';
+    } else if (X == 'consultation') {
 
-    } else if (X == 'consulte-B') {
+        document.getElementById("Travaux-dentaire").style.display = 'none';
+        document.getElementById("Consultation-normale").style.display = 'block';
+        document.getElementById("soin-dentaire").style.display = 'none';
+        document.getElementById("Garantie").style.display = 'block';
 
-        constu1.style.display = 'block';
-        constu2.style.display = 'none';
-        constu3.style.display = 'none';
+    } else if (X == 'soindentaire') {
 
-    } else if (X == 'consulte-C') {
-
-        constu3.style.display = 'block';
-        constu1.style.display = 'none';
-        constu2.style.display = 'none';
+        document.getElementById("Travaux-dentaire").style.display = 'none';
+        document.getElementById("Consultation-normale").style.display = 'none';
+        document.getElementById("soin-dentaire").style.display = 'block';
+        document.getElementById("Garantie").style.display = 'block';
 
     } else {
     }
-    return
+    return;
 }
+
+
+activitie.addEventListener("change", function () {
+    var displayText = activitie.options[activitie.selectedIndex].value;
+    if (displayText == "Couronnes") {
+        document.getElementById('Nbr').style.display = "block";
+        document.getElementById('Nbr').setAttribute("placeholder", "Nbr de couronne");
+        document.getElementById('Nbr').setAttribute("required", "required");
+    }
+    if (displayText == "Bridge 3elements") {
+        document.getElementById('Nbr').style.display = "block";
+        document.getElementById('Nbr').setAttribute("placeholder", "Nbre de Bridge");
+    }
+    if (displayText == "Travaux dorthodontie") {
+        document.getElementById('Nbr').style.display = "block";
+        document.getElementById('Nbr').setAttribute("placeholder", "Nbre de semestre");
+    }
+    if (displayText == "Appareil dentaire(1a3dents)" || displayText == "Appareil dentaire(14dents)") {
+        document.getElementById('Nbr').style.display = "none";
+        document.getElementById('Nbr').removeAttribute("required");
+    }
+});
